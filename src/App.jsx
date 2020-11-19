54af7500c370af579bf64e5d8d9a8a25417c173d
Marcha <marchaf@gmail.com>
2020-11-19 17:40:18 +0100

    Add routes for Create pages(owner, receipt, rental, tenant)

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Profile from "./pages/Profile";
+import CreateReceipt from "./pages/CreateReceipt";
+import CreateTenant from "./pages/CreateTenant";
+import CreateOwner from "./pages/CreateOwner";
+import CreateRental from "./pages/CreateRental";
 
 function App() {
   return (
@@ -16,6 +20,11 @@ function App() {
         <Route exact path="/signin" component={Signin} />
         <Route exact path="/signup" component={Signup} />
         <ProtectedRoute exact path="/profile" component={Profile} />
+        
+        <Route exact path="/receipts/new" component={CreateReceipt} />
+        <Route exact path="/tenant/new" component={CreateTenant} />
+        <Route exact path="/owner/new" component={CreateOwner} />
+        <Route exact path="/rental/new" component={CreateRental} />
       </Switch>
     </div>
   );
